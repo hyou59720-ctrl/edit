4a89b93ec8d4379b90df11efc17d244e57fccb35
hyou59720-ctrl <hyou59720@gmail.com>
2026-08-14 11:51:09 +0300

    Create index.ts for font file exports
    Add direct exports for various font files and FontRenderer.

---
+++ src/project/Components/TextFonts/Font/index.ts
@@ -0,0 +1,14 @@
+// የፎንት ፋይሎችን ቀጥታ Export ማድረግ
+export { default as akiraFont } from "./Font/Akira Expanded Demo.otf";
+export { default as habeshaFont } from "./Font/HabeshaSTENCIL.ttf";
+export { default as inkfreeFont } from "./Font/Inkfree.ttf";
+export { default as surGraphicsFont } from "./Font/Sur_Graphics_Extra_Bold_15c4e92309.ttf";
+export { default as waldbaYebseFont } from "./Font/Waldba_Yebse_Regular_299c8ff034.ttf";
+export { default as waldbaYigezuFont } from "./Font/Waldba_Yigezu_Bisrat_Goffer_Regular_a136e1fd13.ttf";
+export { default as yeshuahFont } from "./Font/YESHUAH.otf";
+export { default as montserratFont } from "./Font/montserrat.black.ttf";
+export { default as tacticSans } from "./Font/TacticSans-Bld.otf";
+
+// FontRenderer-ን Export ማድረግ
+export { FontRenderer } from "./FontRendere";
+
